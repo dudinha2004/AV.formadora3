7783013d94f7a478d6d856d082bf11a210f961e3
dudinha2004 <eduarda.araujo@souunisuam.com.br>
2024-05-22 21:02:31 -0300

    codigo novo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  cep: string | undefined;
+  endereco: any = {};
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  buscarEndereco() {
+    if (this.cep) {
+      const cep = this.cep.replace(/\D/g, '');
+      if (cep.length === 8) {
+        this.http.get<any>(`https://viacep.com.br/ws/${cep}/json/`).subscribe(data => {
+          if (!data.erro) {
+            this.endereco = data;
+          } else {
+            alert('CEP não encontrado.');
+            this.endereco = {};
+          }
+        }, error => {
+          alert('Erro ao buscar o CEP.');
+          console.error(error);
+          this.endereco = {};
+        });
+      } else {
+        alert('CEP inválido.');
+        this.endereco = {};
+      }
+    }
+  }
 }
